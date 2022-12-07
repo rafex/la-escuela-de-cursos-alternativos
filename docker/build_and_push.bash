87f7dad66330a8cbb6044fcc6e4fa2534f3f107d
rafex <rafex@rafex.dev>
2022-12-06 21:22:03 -0600

    flujo nuevo 11

--- docker/build_and_push.bash
+++ docker/build_and_push.bash
@@ -8,10 +8,10 @@ NAME_AND_TAG_IMAGE=$NAME_IMAGE:$TAG_IMAGE
 echo "Docker Auth"
 cat gcp_service.json | docker login -u _json_key --password-stdin https://gcr.io
 echo "Docker Build"
-cd ../alternative-courses
+cd ../alternative-courses > /dev/null
 ls -la
-#docker build . -t $NAME_AND_TAG_IMAGE && docker images
-#echo "Docker Tag"
-#docker tag $NAME_AND_TAG_IMAGE gcr.io/${PROJECT_ID_GCP}/$NAME_IMAGE
-#echo "Docker Push"
-#docker push gcr.io/${PROJECT_ID_GCP}/$NAME_IMAGE
+docker build . -t $NAME_AND_TAG_IMAGE && docker images
+echo "Docker Tag"
+docker tag $NAME_AND_TAG_IMAGE gcr.io/${PROJECT_ID_GCP}/$NAME_IMAGE
+echo "Docker Push"
+docker push gcr.io/${PROJECT_ID_GCP}/$NAME_IMAGE
