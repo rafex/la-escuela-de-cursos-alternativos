2b91984d8b373df64a93346c1fea82c57bd9c7d8
rafex <rafex@rafex.dev>
2022-12-07 00:50:09 -0600

    crealo

--- docker/build_and_push.bash
+++ docker/build_and_push.bash
@@ -14,7 +14,7 @@ ls -la
 cat gcp_service.json
 
 echo "Docker Auth"
-echo $JSON_GCP | docker login -u _json_key_base64 --password-stdin https://gcr.io
+cat gcp_service.json | docker login -u _json_key --password-stdin https://gcr.io
 echo "Docker Build"
 #cd alternative-courses
 ls -la
