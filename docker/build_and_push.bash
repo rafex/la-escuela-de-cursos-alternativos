154253315816c5286bee191b1077ed52f52cedd5
rafex <rafex@rafex.dev>
2022-12-06 23:48:37 -0600

    deploy optimizado 02

--- docker/build_and_push.bash
+++ docker/build_and_push.bash
@@ -8,9 +8,10 @@ NAME_AND_TAG_IMAGE=$NAME_IMAGE:$TAG_IMAGE
 echo "Docker Auth"
 cat gcp_service.json | docker login -u _json_key --password-stdin https://gcr.io
 echo "Docker Build"
-cd alternative-courses
+#cd alternative-courses
 ls -la
-docker build . -t $NAME_AND_TAG_IMAGE && docker images
+docker build . -t $NAME_AND_TAG_IMAGE -f docker/Dockerfile
+docker images
 echo "Docker Tag"
 docker tag $NAME_AND_TAG_IMAGE gcr.io/${PROJECT_ID_GCP}/$NAME_AND_TAG_IMAGE
 echo "Docker Push"
