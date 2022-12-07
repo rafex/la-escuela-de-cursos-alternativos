f16028044f28b5757cc1db41577447f082a13067
rafex <rafex@rafex.dev>
2022-12-07 00:11:25 -0600

    deploy optimizado 03

--- docker/build_and_push.bash
+++ docker/build_and_push.bash
@@ -2,11 +2,15 @@
 
 NAME_IMAGE=$1
 PROJECT_ID_GCP=$2
+JSON_GCP=$3
 TAG_IMAGE=$(date +%s)
 NAME_AND_TAG_IMAGE=$NAME_IMAGE:$TAG_IMAGE
 
+echo "Creando JSON GCP"
+echo $JSON_GCP | base64 -d > gcp_service.json
+
 echo "Docker Auth"
-cat gcp_service.json | docker login -u _json_key --password-stdin https://gcr.io
+echo $JSON_GCP | docker login -u _json_key_base64 --password-stdin https://gcr.io
 echo "Docker Build"
 #cd alternative-courses
 ls -la
@@ -17,9 +21,8 @@ docker tag $NAME_AND_TAG_IMAGE gcr.io/${PROJECT_ID_GCP}/$NAME_AND_TAG_IMAGE
 echo "Docker Push"
 docker push gcr.io/${PROJECT_ID_GCP}/$NAME_AND_TAG_IMAGE
 
-echo "Deploy GKE"
-
-sudo snap install google-cloud-cli --classic
-gcloud auth activate-service-account --key-file=gcp_service.json --project=${PROJECT_ID_GCP}
-gcloud container clusters get-credentials cluster-1 --region us-central1-c
-kubectl set image deployment nginx-1 tutum-demo-1-1=gcr.io/${PROJECT_ID_GCP}/$NAME_AND_TAG_IMAGE
+#echo "Deploy GKE"
+#sudo snap install google-cloud-cli --classic
+#gcloud auth activate-service-account --key-file=gcp_service.json --project=${PROJECT_ID_GCP}
+#gcloud container clusters get-credentials cluster-1 --region us-central1-c
+#kubectl set image deployment nginx-1 tutum-demo-1-1=gcr.io/${PROJECT_ID_GCP}/$NAME_AND_TAG_IMAGE
