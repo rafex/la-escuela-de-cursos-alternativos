ee78e996b6c26e083a53d832e91e1b581a4bb086
rafex <rafex@rafex.dev>
2022-12-07 19:13:10 -0600

    insert data init

---
+++ sql/flyway/sql/prod/V20221207.100000__insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO t_alumnos VALUES (nextval('s_alumno'),'John','Dow','Down',true);
+
+INSERT INTO t_materias VALUES (nextval('s_materia'),'matematicas',true);
+INSERT INTO t_materias VALUES (nextval('s_materia'),'programacion',true);
+INSERT INTO t_materias VALUES (nextval('s_materia'),'ingenieria de sofware',true);
