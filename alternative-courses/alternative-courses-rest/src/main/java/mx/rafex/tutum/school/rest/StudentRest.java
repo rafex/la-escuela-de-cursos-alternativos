14b2eb5ea7759dacccd04cd3d797acdbfcfdf023
rafex <rafex@rafex.dev>
2022-12-07 20:40:44 -0600

    vamos

--- alternative-courses/alternative-courses-rest/src/main/java/mx/rafex/tutum/school/rest/StudentRest.java
+++ alternative-courses/alternative-courses-rest/src/main/java/mx/rafex/tutum/school/rest/StudentRest.java
@@ -2,8 +2,17 @@ package mx.rafex.tutum.school.rest;
 
 import java.util.logging.Logger;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 public interface StudentRest {
 
     Logger LOG = Logger.getLogger(StudentRest.class.getName());
+
+    @RequestMapping(method = { RequestMethod.GET })
+    ResponseEntity<?> list(
+            @RequestParam(required = true, name = "startDate") String startDate);
 
 }
