80b50790d911c304e116263adecaa22107e81886
rafex <rafex@rafex.dev>
2022-12-08 14:51:44 -0600

    desplegando

--- docker/build_and_push.bash
+++ docker/build_and_push.bash
@@ -1,10 +1,12 @@
 #! /bin/bash
 
 NAME_IMAGE=$1
-PROJECT_ID_GCP=$2
-JSON_GCP=$3
+NAME_IMAGE_2=$2
+PROJECT_ID_GCP=$3
+JSON_GCP=$4
 TAG_IMAGE=$(date +%s)
 NAME_AND_TAG_IMAGE=$NAME_IMAGE:$TAG_IMAGE
+NAME_AND_TAG_IMAGE_2=$NAME_IMAGE_2:$TAG_IMAGE
 
 echo "Creando JSON GCP"
 echo $JSON_GCP | base64 -d > gcp_service.json
@@ -15,10 +17,10 @@ cat gcp_service.json
 
 echo "Docker Auth"
 cat gcp_service.json | docker login -u _json_key --password-stdin https://gcr.io
-echo "Docker Build"
+echo "Docker Build Webapp"
 #cd alternative-courses
 ls -la
-docker build . -t $NAME_AND_TAG_IMAGE -f docker/Dockerfile
+docker build . -t $NAME_AND_TAG_IMAGE -f docker/Dockerfile.webapp
 docker images
 echo "Docker Tag"
 docker tag $NAME_AND_TAG_IMAGE gcr.io/${PROJECT_ID_GCP}/$NAME_AND_TAG_IMAGE
@@ -27,6 +29,19 @@ docker push gcr.io/${PROJECT_ID_GCP}/$NAME_AND_TAG_IMAGE
 
 echo "Deploy GKE"
 kubectl set image deployment tutum-demo-1 tutum-demo-1-sha256-1=gcr.io/${PROJECT_ID_GCP}/$NAME_AND_TAG_IMAGE
+
+echo "Docker Build Webapp"
+#cd alternative-courses
+ls -la
+docker build . -t $NAME_AND_TAG_IMAGE_2 -f docker/Dockerfile.webservices
+docker images
+echo "Docker Tag"
+docker tag $NAME_AND_TAG_IMAGE_2 gcr.io/${PROJECT_ID_GCP}/$NAME_AND_TAG_IMAGE_2
+echo "Docker Push"
+docker push gcr.io/${PROJECT_ID_GCP}/$NAME_AND_TAG_IMAGE_2
+
+echo "Deploy GKE"
+kubectl set image deployment tutum-demo-rest-1 tutum-demo-1-sha256-1=gcr.io/${PROJECT_ID_GCP}/$NAME_AND_TAG_IMAGE_2
 
 
 
