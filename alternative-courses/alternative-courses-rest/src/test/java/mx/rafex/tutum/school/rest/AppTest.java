e576c0df2795249f5696ba6649724e8692440b8a
rafex <rafex@rafex.dev>
2022-12-19 12:11:08 -0600

    test

--- alternative-courses/alternative-courses-rest/src/test/java/mx/rafex/tutum/school/rest/AppTest.java
+++ alternative-courses/alternative-courses-rest/src/test/java/mx/rafex/tutum/school/rest/AppTest.java
@@ -1,7 +1,15 @@
 package mx.rafex.tutum.school.rest;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
 /**
  * Unit test for simple App.
  */
+@SpringBootTest
 public class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
 }
